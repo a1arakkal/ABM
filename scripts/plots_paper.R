32aba38dfba04e9c6571bc8b4b20533c895dcffa
a1arakkal <alan-arakkal@uiowa.edu>
2025-10-31 13:32:25 -0600

    Update plots_paper.R

--- scripts/plots_paper.R
+++ scripts/plots_paper.R
@@ -360,11 +360,11 @@ fig4 <- main_res %>%
   ggplot(aes(x = factor(p_asym), fill = label)) +
   geom_boxplot(
     aes(
-      ymin = ymin,
-      lower = Q1,
-      middle = median,
-      upper = Q3,
-      ymax = ymax
+      ymin = ymin*100,
+      lower = Q1*100,
+      middle = median*100,
+      upper = Q3*100,
+      ymax = ymax*100
     ),
     stat = "identity",
     alpha = 1
@@ -451,7 +451,7 @@ fig5 <- main_res %>%
   ) +
   labs(
     x = "Asymptomatic probability",
-    y = "Average number of contact per actor"
+    y = "Average number of contacts per actor"
   )+
   geom_vline(xintercept = seq(1.5, length(c(0, 0.2, 0.4, 0.6, 0.8)) - 0.5, by = 1),
              linetype = "dashed", color = "grey50")+
@@ -519,7 +519,7 @@ fig6 <- main_res %>%
   ) +
   labs(
     x = "Asymptomatic probability",
-    y = "Ratio of total infections versus\ntotal number of contacts"
+    y = "Ratio of total infections to\ntotal number of contacts"
   )+
   geom_vline(xintercept = seq(1.5, length(c(0, 0.2, 0.4, 0.6, 0.8)) - 0.5, by = 1),
              linetype = "dashed", color = "grey50")+
@@ -620,7 +620,7 @@ fig7 <- main_res %>%
   filter(cluster_label == 'clusters_accounting_for_noise') %>% 
   filter(quarantine_days == 5) %>% 
   filter(metric == "tot_inf_by_by_contacts") %>% 
-  filter(round(p_asym, 1) %in% c(0.6)) %>% 
+  filter(round(p_asym, 1) %in% c(0.4)) %>% 
   filter(DCT_sensitivity %in% c(0.6, 0.8, 1)) %>%
   filter(DCT_specificity == 1) %>%
   mutate(label = case_when(
@@ -634,6 +634,7 @@ fig7 <- main_res %>%
                          label) ) %>% 
   filter(label != "No Intervention") %>% 
   select(2:7, p_asym, modularity, label) %>% 
+  # mutate(modularity = round(modularity*100)) %>% 
   distinct() %>% 
   mutate(label = factor(label, 
                         levels = c(
@@ -669,12 +670,13 @@ fig7 <- main_res %>%
     text = element_text(size = 14)
   ) +
   labs(
-    x = "Modularity",
-    y = "Ratio of total infections versus\ntotal number of contacts"
+    x = "Proportion of edges within cluster",
+    y = "Ratio of total infections to\ntotal number of contacts"
   )+
   geom_vline(xintercept = seq(1.5, length(seq(0, 1, 0.1)) - 0.5, by = 1),
              linetype = "dashed", color = "grey50")+
-  guides(fill = guide_legend(nrow = 2, byrow = TRUE))
+  guides(fill = guide_legend(nrow = 2, byrow = TRUE))+
+  scale_y_continuous(labels = scales::scientific)
 
 
 jpeg("/Volumes/argon_home/dissertation/real_data_application/paper3/figures/fig7.jpeg",
@@ -761,7 +763,7 @@ fig8 <- main_res %>%
   filter(cluster_label == 'clusters_fb_network') %>% 
   filter(quarantine_days == 5) %>% 
   filter(metric == "tot_inf_by_by_contacts") %>% 
-  filter(round(p_asym, 1) %in% c(0.6)) %>% 
+  filter(round(p_asym, 1) %in% c(0.4)) %>% 
   filter(DCT_sensitivity %in% c(0.6, 0.8, 1)) %>%
   filter(DCT_specificity == 1) %>%
   mutate(label = case_when(
@@ -776,9 +778,7 @@ fig8 <- main_res %>%
   filter(label != "No Intervention") %>% 
   select(2:7, p_asym, modularity, label) %>% 
   distinct() %>% 
-  group_by(p_asym, modularity, label) %>%
-  filter(median == min(median)) %>%
-  ungroup() %>%
+  # mutate(modularity = round(modularity*100)) %>%  
   mutate(label = factor(label, 
                         levels = c(
                           "No Intervention",
@@ -813,15 +813,413 @@ fig8 <- main_res %>%
     text = element_text(size = 14)
   ) +
   labs(
-    x = "Modularity",
-    y = "Ratio of total infections versus\ntotal number of contacts"
+    x = "Proportion of edges within cluster",
+    y = "Ratio of total infections to\ntotal number of contacts"
   )+
   geom_vline(xintercept = seq(1.5, length(seq(0, 1, 0.1)) - 0.5, by = 1),
              linetype = "dashed", color = "grey50")+
-  guides(fill = guide_legend(nrow = 2, byrow = TRUE))
+  guides(fill = guide_legend(nrow = 2, byrow = TRUE))+
+  scale_y_continuous(labels = scales::scientific)
 
 
 jpeg("/Volumes/argon_home/dissertation/real_data_application/paper3/figures/fig8.jpeg",
      width = 6.1, height = 4.8, units = "in", res = 1000)
 grid::grid.draw(fig8)
 dev.off()
+
+# Example of change modularity -------------------------------------------------
+
+setwd("/Volumes/argon_home/dissertation/real_data_application/paper3/")
+
+load(file = "int_and_neighbors_by_t.RData")
+
+int_and_neighbors_by_t_true <- int_and_neighbors_by_t["20"]
+
+# Load in LSHM results
+
+load(file = "cns_week_1_2_res.RData")
+
+clusters_accounting_for_noise <- JANE:::summary.JANE(cns_fits)$cluster_labels
+clusters_ignore_noise <- JANE:::summary.JANE(cns_fits_ignore_noise)$cluster_labels
+clusters_dichotomize_g_1 <- JANE:::summary.JANE(cns_fits_di)$cluster_labels
+
+rm(list = setdiff(ls(), c("int_and_neighbors_by_t_true",
+                          "total_actors",
+                          "clusters_accounting_for_noise")))
+
+load(file = "fb_net_LSM_res.RData")
+clusters_fb_network <- JANE:::summary.JANE(cns_fits)$cluster_labels
+
+rm(list = setdiff(ls(), c("int_and_neighbors_by_t_true",
+                          "total_actors",
+                          "clusters_accounting_for_noise",
+                          "clusters_fb_network")))
+
+# Source helper functions 
+
+modularity <- function(network,
+                       total_actors,
+                       assumed_true_communities){
+  
+  out <-  sapply(names(network), 
+                 function(x){
+                   
+                   A <- matrix(0, nrow = total_actors, ncol= total_actors)
+                   colnames(A) <- 1:total_actors
+                   rownames(A) <- 1:total_actors
+                   dat <- network[[x]]$neighbors
+                   
+                   for(i in names(dat)){
+                     A[i, names(dat[[i]])] <- 1
+                   }
+                   
+                   sum_within <- 0
+                   for (i in unique(assumed_true_communities)){
+                     sum_within <- sum_within + sum(A[names(which(assumed_true_communities == i)),
+                                                      names(which(assumed_true_communities == i))])
+                   }
+                   
+                   sum_between <- sum(A) - sum_within
+                   
+                   return(c(sum_within, sum_between, sum_within/(sum(A))))
+                 })
+  
+}
+
+modularity_clusters_accounting_for_noise <- modularity(int_and_neighbors_by_t_true,
+                                                       total_actors,
+                                                       clusters_accounting_for_noise)
+
+modularity_clusters_fb_network <- modularity(int_and_neighbors_by_t_true,
+                                             total_actors,
+                                             clusters_fb_network)
+
+# Function to adjust modularity ------------------------------------------------
+
+adjust_modularity <- function(network,
+                              total_actors,
+                              assumed_true_communities,
+                              prob_within,
+                              tol = 1e-2){
+  
+  new_network <- vector("list", length = length(network))
+  names(new_network) <- names(network)
+  modularity <- numeric(length = length(network))
+  names(modularity) <- names(network)
+  
+  for (t in names(network)){
+    
+    # extract within and between edges -----------------------------------------
+    A <- matrix(0, nrow = total_actors, ncol= total_actors)
+    colnames(A) <- 1:total_actors
+    rownames(A) <- 1:total_actors
+    dat <- network[[t]]$neighbors
+    
+    for(i in names(dat)){
+      A[i, names(dat[[i]])] <- unname(dat[[i]])
+    }
+    
+    # isSymmetric(A)
+    indices_w_ints <- which(A>0, arr.ind = TRUE)
+    indices_w_ints <- cbind(indices_w_ints, 0)
+    colnames(indices_w_ints) <- c("i", "j", "within")
+    indices_w_ints <- indices_w_ints[indices_w_ints[, "j"] > indices_w_ints[, "i"], ]
+    indices_w_ints <- cbind(indices_w_ints, A[indices_w_ints[, 1:2]])
+    colnames(indices_w_ints) <- c("i", "j", "within", "value")
+    
+    within_loc <- (as.character(indices_w_ints[,"i"]) %in% names(assumed_true_communities)) & 
+      (as.character(indices_w_ints[,"j"]) %in% names(assumed_true_communities)) &
+      (assumed_true_communities[as.character(indices_w_ints[,"i"])] ==  assumed_true_communities[as.character(indices_w_ints[,"j"])])
+    within_loc[is.na(within_loc)] <- FALSE
+    indices_w_ints[within_loc, "within"] <- 1
+    
+    obs_p_within <- prop.table(table(indices_w_ints[, "within"]))["1"]
+    within_edges  <- indices_w_ints[indices_w_ints[, "within"] == 1, ]
+    between_edges <-  indices_w_ints[indices_w_ints[, "within"] == 0, ]
+    
+    if(!is.null(prob_within)){
+      
+      if(prob_within == 1){
+        
+        out <- within_edges
+        
+      } else if (prob_within == 0){
+        
+        out <- between_edges
+        
+      } else if ( abs(prob_within - obs_p_within) < tol){
+        
+        out <- indices_w_ints
+        
+      } else {
+        
+        if (obs_p_within < prob_within) {
+          
+          # need to remove some between edges
+          within <- within_edges
+          N_between <- round((nrow(within) * (1 - prob_within)) / prob_within)
+          between <- between_edges[order(runif(nrow(between_edges))), ]
+          out <- rbind(within, between[1:N_between, ])
+          
+        } else {
+          
+          # need to remove some within edges
+          between <- between_edges
+          N_within <- round((prob_within * nrow(between)) / (1 - prob_within))
+          within <- within_edges[order(runif(nrow(within_edges))), ]
+          out <- rbind(within[1:N_within, ], between)
+          
+        }
+        
+      } 
+      
+    } else {
+      
+      out <- indices_w_ints
+      
+    }
+    
+    B <- matrix(0, nrow = total_actors, ncol= total_actors)
+    colnames(B) <- 1:total_actors
+    rownames(B) <- 1:total_actors
+    
+    B[out[, 1:2]] <- out[, "value"]
+    B[out[, 2:1]] <- out[, "value"]
+    # isSymmetric(B)
+    n_total_t <- rowSums(B)
+    check_isolates <- n_total_t == 0
+    
+    sum_within <- 0
+    for (i in unique(assumed_true_communities)){
+      sum_within <- sum_within + sum((B>0)[names(which(assumed_true_communities == i)),
+                                           names(which(assumed_true_communities == i))])
+    }
+    modularity[t] <- sum_within/sum(B>0)
+    # modularity[t] <- prop.table(table(out[, "within"]))[["1"]]
+    
+    temp2 <- lapply(rownames(B)[!check_isolates], function(y){
+      z <- B[y, ]
+      z <- z[z!=0]
+      return(z)
+    })
+    
+    names(temp2) <- rownames(B)[!check_isolates]
+    
+    new_network[[t]] <- list(neighbors = temp2,
+                             n_total_t = n_total_t[!check_isolates])
+    
+  }
+  
+  return(list(network = new_network,
+              modularity = modularity))
+  
+}
+
+add_cluster_boundaries <- function(p, cluster_vec) {
+  boundaries <- cumsum(table(cluster_vec))
+  n <- length(cluster_vec)
+  
+  for (b in boundaries[-length(boundaries)]) {
+    p <- p +
+      geom_vline(xintercept = b, linetype="solid", 
+                 color = "black", size=0.5)+
+      geom_hline(yintercept = n - b, linetype="solid", 
+                 color = "black", size=0.5)
+  }
+  return(p)
+}
+
+plot_fun <- function(network,
+                     total_actors,
+                     assumed_true_communities,
+                     prob_within,
+                     tol = 1e-2,
+                     t){
+
+  if (is.null(prob_within)){
+
+    A <- matrix(0, nrow = total_actors, ncol= total_actors)
+    colnames(A) <- 1:total_actors
+    rownames(A) <- 1:total_actors
+    dat <- network[[t]]$neighbors
+
+    for(i in names(dat)){
+      A[i, names(dat[[i]])] <- unname(dat[[i]])
+    }
+    
+    } else {
+
+    temp <- adjust_modularity(network = network,
+                              total_actors = total_actors,
+                              assumed_true_communities = assumed_true_communities,
+                              prob_within = prob_within,
+                              tol = tol-2)
+
+    A <- matrix(0, nrow = total_actors, ncol= total_actors)
+    colnames(A) <- 1:total_actors
+    rownames(A) <- 1:total_actors
+    dat <- temp$network[[t]]$neighbors
+
+    for(i in names(dat)){
+      A[i, names(dat[[i]])] <- unname(dat[[i]])
+    }
+
+  }
+
+  #cns_fits_di
+  plot_mat <- as.matrix(A)
+  order_clust <- sort(assumed_true_communities)
+  
+  adj_plot <- as.data.frame(as.table(plot_mat)) %>%
+    rename(row = Var1, col = Var2) %>% 
+    mutate(value = cut(Freq, breaks = c(-1, 0, Inf),
+                       labels = c("0", ">0"))) %>% 
+    mutate(
+      row = factor(row, levels = rev(names(order_clust))),
+      col = factor(col, levels = names(order_clust))
+    ) %>% 
+    ggplot(aes(x = col, y = row, fill = value)) +
+    geom_tile() +
+    scale_fill_manual(values = c("0" = "white", ">0" = "#FF6A33")) + 
+    geom_tile() +
+    theme_minimal() +
+    theme(
+      axis.text = element_blank(),
+      axis.ticks = element_blank(),
+      panel.grid = element_blank(),
+      legend.position = "none",
+      plot.title = element_text(hjust = 0.5,
+                                size = 12)
+    ) +
+    ylab("")+
+    xlab("")
+  
+  adj_plot <- add_cluster_boundaries(adj_plot, order_clust) 
+  
+  return(adj_plot)
+}
+
+p1 <- plot_fun(network = int_and_neighbors_by_t_true,
+         total_actors = total_actors,
+         assumed_true_communities = clusters_accounting_for_noise,
+         prob_within = NULL,
+         t = "20") +
+  ggtitle("(A)")
+
+
+p2 <- plot_fun(network = int_and_neighbors_by_t_true,
+               total_actors = total_actors,
+               assumed_true_communities = clusters_accounting_for_noise,
+               prob_within = 0,
+               t = "20") +
+  ggtitle("(B)")
+
+
+p3 <- plot_fun(network = int_and_neighbors_by_t_true,
+               total_actors = total_actors,
+               assumed_true_communities = clusters_accounting_for_noise,
+               prob_within = .5,
+               t = "20") +
+  ggtitle("(C)")
+
+
+p4 <- plot_fun(network = int_and_neighbors_by_t_true,
+               total_actors = total_actors,
+               assumed_true_communities = clusters_accounting_for_noise,
+               prob_within = 1,
+               t = "20") +
+  ggtitle("(D)")
+
+
+# Arrange 
+fig9 <- grid.arrange(
+  arrangeGrob(p1, p2, ncol = 2),
+  arrangeGrob(p3, p4, ncol = 2),
+  ncol = 1,
+  heights = c(2, 2)
+)
+
+jpeg("/Volumes/argon_home/dissertation/real_data_application/paper3/figures/fig9.jpeg",
+     width = 6.1, height = 4.8, units = "in", res = 1000)
+grid::grid.draw(fig9)
+dev.off()
+
+
+
+p1 <- plot_fun(network = int_and_neighbors_by_t_true,
+               total_actors = total_actors,
+               assumed_true_communities = clusters_fb_network,
+               prob_within = NULL,
+               t = "20") +
+  ggtitle("(A)")
+
+
+p2 <- plot_fun(network = int_and_neighbors_by_t_true,
+               total_actors = total_actors,
+               assumed_true_communities = clusters_fb_network,
+               prob_within = 0,
+               t = "20") +
+  ggtitle("(B)")
+
+
+p3 <- plot_fun(network = int_and_neighbors_by_t_true,
+               total_actors = total_actors,
+               assumed_true_communities = clusters_fb_network,
+               prob_within = .5,
+               t = "20") +
+  ggtitle("(C)")
+
+
+p4 <- plot_fun(network = int_and_neighbors_by_t_true,
+               total_actors = total_actors,
+               assumed_true_communities = clusters_fb_network,
+               prob_within = 1,
+               t = "20") +
+  ggtitle("(D)")
+
+
+# Arrange 
+fig10 <- grid.arrange(
+  arrangeGrob(p1, p2, ncol = 2),
+  arrangeGrob(p3, p4, ncol = 2),
+  ncol = 1,
+  heights = c(2, 2)
+)
+
+jpeg("/Volumes/argon_home/dissertation/real_data_application/paper3/figures/fig10.jpeg",
+     width = 6.1, height = 4.8, units = "in", res = 1000)
+grid::grid.draw(fig10)
+dev.off()
+
+
+### Data for text --------------------------------------------------------------
+
+# Load in interactions by time list --------------------------------------------
+
+setwd("/Volumes/argon_home/dissertation/real_data_application/paper3/")
+
+# Load in LSHM results --------------------------------------------
+
+load(file = "cns_week_1_2_res.RData")
+
+clusters_accounting_for_noise <- JANE:::summary.JANE(cns_fits)$cluster_labels
+clusters_ignore_noise <- JANE:::summary.JANE(cns_fits_ignore_noise)$cluster_labels
+clusters_dichotomize_g_1 <- JANE:::summary.JANE(cns_fits_di)$cluster_labels
+
+isolates <- 692-nrow(cns_fits$A)
+sum(cns_fits$A>0)/2
+sum(cns_fits$A>0)/(nrow(cns_fits$A)*(nrow(cns_fits$A)-1))
+length(table((clusters_accounting_for_noise)))
+sort(table(clusters_accounting_for_noise))
+rm(list = setdiff(ls(), c("int_and_neighbors_by_t_true",
+                          "total_actors",
+                          "clusters_accounting_for_noise")))
+
+load(file = "fb_net_LSM_res.RData")
+clusters_fb_network <- JANE:::summary.JANE(cns_fits)$cluster_labels
+
+isolates <- 692-nrow(cns_fits$A)
+sum(cns_fits$A>0)/2
+sum(cns_fits$A>0)/(nrow(cns_fits$A)*(nrow(cns_fits$A)-1))
+length(table((clusters_fb_network)))
+sort(table(clusters_fb_network))
